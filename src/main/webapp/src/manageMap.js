4e69d4c9bbab73e44cbdcba34e5ddb5768f61f23
alexandr <Subwan@gmail.com>
2017-11-07 11:05:35 +0300

    Add draw/modify function. Add change map type (OSM, Google map).

--- src/main/webapp/src/manageMap.js
+++ src/main/webapp/src/manageMap.js
@@ -1,1 +1,24 @@
-var map = new ol.Map({target: 'map'});
+var map = new ol.Map({
+  target: 'map',
+    controls: ol.control.defaults({
+    attributionOptions: {
+      collapsible: false
+    }
+  }),
+    view: new ol.View({
+      center: [0, 0],
+      zoom: 0
+    }),
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.TileWMS({
+        projection: 'EPSG:4326', //HERE IS THE DATA SOURCE PROJECTION
+        url: 'http://demo.boundlessgeo.com/geoserver/wms',
+        params: {
+          'LAYERS': 'ne:NE1_HR_LC_SR_W_DR'
+        }
+      })
+      })
+    ]
+
+  });
